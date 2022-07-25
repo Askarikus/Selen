27d774800267bb2c522d30aa2af4cecf5551fcbc
Askarikus <askarikus@yandex.ru>
2022-07-25 16:27:02 +0500

    I can enter to https://web.whatsapp.com through firefox

--- chromedriver/main_chrome.py
+++ chromedriver/main_chrome.py
@@ -2,11 +2,11 @@ from selenium import webdriver
 import time
 
 driver = webdriver.Chrome(executable_path='/home/askar/Py/Selen/chromedriver/chromedriver')
-url = 'https://www.google.com/'
+url = 'https://web.whatsapp.com/'
 
 try:
     driver.get(url=url)
-    time.sleep(2)
+    time.sleep(60)
 
 except Exception as e:
     print(e)
--- geckodriver/main_firefox.py
+++ geckodriver/main_firefox.py
@@ -6,11 +6,11 @@ options = Options()
 options.add_argument("-profile")
 options.add_argument("/home/askar/snap/firefox/common/.mozilla/firefox/tc2j6nxr.default-release")
 driver = webdriver.Firefox(executable_path='/home/askar/Py/Selen/geckodriver/geckodriver', options=options)
-url = 'https://www.google.com/'
+url = 'https://web.whatsapp.com/'
 
 try:
     driver.get(url=url)
-    time.sleep(2)
+    time.sleep(60)
 
 except Exception as e:
     print(e)
