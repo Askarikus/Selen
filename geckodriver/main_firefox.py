1a5a529106915044eb12a4f0836d3a27ed6bfeb7
Askarikus <askarikus@yandex.ru>
2022-07-26 16:39:12 +0500

    recieve last message in dialog

--- geckodriver/main_firefox.py
+++ geckodriver/main_firefox.py
@@ -1,44 +1,7 @@
 from selenium import webdriver
-from selenium.webdriver import Keys
-from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.support import expected_conditions as EC
-import time
-
-from selenium.webdriver.support.wait import WebDriverWait
 
 options = Options()
 options.add_argument("-profile")
 options.add_argument("/home/askar/snap/firefox/common/.mozilla/firefox/tc2j6nxr.default-release")
 driver = webdriver.Firefox(executable_path='/home/askar/Py/Selen/geckodriver/geckodriver', options=options)
-url = 'https://web.whatsapp.com/'
-
-try:
-    driver.get(url=url)
-    time.sleep(20)
-    contact = "whatbot"
-    input_box_search_xpath = "//div[@data-testid='chat-list-search']"
-    input_box_search = WebDriverWait(driver, 30).until(
-        lambda driver: driver.find_element(By.XPATH, input_box_search_xpath))
-    input_box_search.click()
-    time.sleep(10)
-    input_box_search.send_keys(contact)
-    time.sleep(10)
-    selected_contact = driver.find_element(By.XPATH, "//span[@title='" + contact + "']")
-    selected_contact.click()
-    time.sleep(10)
-    inp_xpath = "//div[@title='Введите сообщение']"
-    input_box = driver.find_element(By.XPATH, inp_xpath)
-    time.sleep(2)
-    # input_box.send_keys("#" + Keys.ENTER)
-    input_box.send_keys("#")
-    btn_send_xpath = "//button[@data-testid='compose-btn-send']"
-    btn_send = driver.find_element(By.XPATH, btn_send_xpath)
-    btn_send.click()
-    time.sleep(20)
-    # driver.quit()
-except Exception as e:
-    print(e)
-finally:
-    driver.close()
-    driver.quit()
---
+++ tests/test_main_menu.py
@@ -0,0 +1,52 @@
+from datetime import time
+
+import pytest
+from geckodriver.main_firefox import driver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+import time
+
+
+def test_main_menu_send_sharp():
+    url = 'https://web.whatsapp.com/'
+    try:
+        # открываем страницу
+        driver.get(url=url)
+        time.sleep(30)
+        contact = "whatbot"
+        text = "#"
+        # ждем пока не подгрузятся элементы ввода
+        input_box_search_xpath = "//div[@data-testid='chat-list-search']"
+        input_box_search = WebDriverWait(driver, 30).until(
+            lambda driver: driver.find_element(By.XPATH, input_box_search_xpath))
+        # отправляем в поле ввода наименование контакт(бота)
+        input_box_search.click()
+        time.sleep(10)
+        input_box_search.send_keys(contact)
+        time.sleep(10)
+        # кликаем на поле контакт
+        selected_contact = driver.find_element(By.XPATH, "//span[@title='" + contact + "']")
+        selected_contact.click()
+        time.sleep(10)
+        # находим элемент поле ввода сообщения
+        inp_xpath = "//div[@title='Введите сообщение']"
+        input_box = driver.find_element(By.XPATH, inp_xpath)
+        time.sleep(2)
+        # input_box.send_keys("#" + Keys.ENTER)
+        # вводим заранее сохраненный текст
+        input_box.send_keys(text)
+        btn_send_xpath = "//button[@data-testid='compose-btn-send']"
+        # жмём отправить
+        btn_send = driver.find_element(By.XPATH, btn_send_xpath)
+        btn_send.click()
+        time.sleep(20)
+        messages_xpath = "//div[@class='_2wUmf message-in focusable-list-item']"
+        last_messages = driver.find_elements(By.XPATH, messages_xpath)[-1]
+        assert "Привет.\n\nНа связи бот сервиса Wahelp" == last_messages.text[:38]
+        # driver.quit()
+
+    except Exception as e:
+        print(e)
+    finally:
+        driver.close()
+        driver.quit()
